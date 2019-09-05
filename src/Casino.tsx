0dd5d080f2babd8fc7bcd33ad65a97c147b198e6
Erick Gómez <erick0gomez98@gmail.com>
2019-09-05 16:52:09 -0500

    Se agregó la clase de casino que se encargará de ser un wrapper de toda la funcionalidad del programa.

---
+++ src/Casino.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type CasinoProps = {};
+type CasinoState = {};
+
+export default class Casino extends React.Component<CasinoProps, CasinoState> {
+    render(){
+        return (
+            <h1>Es un casino</h1>
+        )
+    }
+}
