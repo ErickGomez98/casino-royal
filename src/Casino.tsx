a20a9591b31abe08703342ea000f87ae7536589e
Erick <erick0gomez98@gmail.com>
2019-09-06 14:20:21 -0500

    Se agregaron clases de css

--- src/Casino.tsx
+++ src/Casino.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import CasinoJuego from './CasinoJuego';
 import CasinoControls from './CasinoControls';
 
@@ -8,9 +9,13 @@ type CasinoState = {};
 export default class Casino extends React.Component<CasinoProps, CasinoState> {
     render() {
         return (
-            <div>
-                <CasinoControls />
-                <CasinoJuego />
+            <div className="casino-main-container">
+                <div className="casino-controls-container">
+                    <CasinoControls />
+                </div>
+                <div className="casino-juego-container">
+                    <CasinoJuego />
+                </div>
             </div>
         )
     }
