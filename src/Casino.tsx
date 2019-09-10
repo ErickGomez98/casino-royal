3f8c058371079700439b8cb88178d4cd116104f2
Erick Gomez <erick0gomez98@gmail.com>
2019-09-10 17:38:12 -0500

    Se hicieron nuevos components y se cambio de fn a class el component de casinoJuego.

--- src/Casino.tsx
+++ src/Casino.tsx
@@ -4,17 +4,23 @@ import CasinoJuego from './CasinoJuego';
 import CasinoControls from './CasinoControls';
 
 type CasinoProps = {};
-type CasinoState = {};
+type CasinoState = {
+    playable: boolean
+};
 
 export default class Casino extends React.Component<CasinoProps, CasinoState> {
+    state: CasinoState = {
+        playable: false
+    };
     render() {
+        const { playable } = this.state;
         return (
             <div className="casino-main-container">
                 <div className="casino-controls-container">
                     <CasinoControls />
                 </div>
                 <div className="casino-juego-container">
-                    <CasinoJuego />
+                    <CasinoJuego jugable={playable} />
                 </div>
             </div>
         )
--- src/CasinoJuego.tsx
+++ src/CasinoJuego.tsx
@@ -13,23 +13,50 @@ const CasinoJuegoSlot: React.FC<any> = (props) => {
     )
 };
 
-const CasinoJuego: React.FC = () => {
+type PalacncaProps = {
+    onClickJugar: React.MouseEvent;
+}
+
+const Palanca: React.FC<any> = (props) => {
     return (
-        <div className="main-juego-container">
-            <div>
-                <h2>Casino Juego</h2>
-            </div>
-            <div className="caja-juego">
-                <CasinoJuegoSlot bg={bell} />
-                <CasinoJuegoSlot bg={seven} />
-                <CasinoJuegoSlot bg={cherry} />
-                <CasinoJuegoSlot bg={bar} />
-                <div className="medio-circulo"></div>
-                <div className="palanca"></div>
-                <div className="palanca-head"></div>
-            </div>
+        <div onClick={() => props.onClickJugar()}>
+            <div className="medio-circulo"></div>
+            <div className="palanca"></div>
+            <div className="palanca-head"></div>
         </div>
     )
+};
+
+
+
+type CasinoJuegoProps = {
+    jugable: boolean;
+}
+
+class CasinoJuego extends React.Component<CasinoJuegoProps, {}> {
+
+    jugar = () => {
+        if (!this.props.jugable) {
+            alert("no hay créditos para jugar");
+        }
+    }
+
+    render() {
+        return (
+            <div className="main-juego-container">
+                <div>
+                    <h2>Casino Juego</h2>
+                </div>
+                <div className="caja-juego">
+                    <CasinoJuegoSlot bg={bell} />
+                    <CasinoJuegoSlot bg={seven} />
+                    <CasinoJuegoSlot bg={cherry} />
+                    <CasinoJuegoSlot bg={bar} />
+                    <Palanca onClickJugar={this.jugar} />
+                </div>
+            </div>
+        )
+    }
 };
 
 export default CasinoJuego;
