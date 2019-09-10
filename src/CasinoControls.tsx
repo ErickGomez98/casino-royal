a372269b146332f1ebf7d9f438ffe6c70c611c0c
Erick Gomez <erick0gomez98@gmail.com>
2019-09-10 15:40:30 -0500

    Se agregaron monedas y se esta trabajando en el estilo de la aplicación.

--- src/CasinoControls.tsx
+++ src/CasinoControls.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 
+const Moneda: React.FC<any> = (props) => {
+    return (
+        <div {...props} className="moneda-txt">{props.denominacion}</div>
+    )
+}
+
 const CasinoControls: React.FC = (props) => {
     return (
-        <h1>Los controles</h1>
+        <div className="main-controles-container">
+            <div className="monedas-container">
+                <div className="moneda">
+                    <Moneda denominacion={2} />
+                </div>
+                <div className="moneda">
+                    <Moneda denominacion={5} />
+                </div>
+                <div className="moneda">
+                    <Moneda denominacion={10} />
+                </div>
+                <div className="moneda">
+                    <Moneda denominacion={20} />
+                </div>
+            </div>
+        </div>
     )
 };
 
