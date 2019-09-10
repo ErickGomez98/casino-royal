fcb7341677c888bb3549b6aed2134c2c0e8328c0
Erick Gomez <erick0gomez98@gmail.com>
2019-09-10 16:41:21 -0500

    Se esta construyendo el juego con puro css

--- src/CasinoJuego.tsx
+++ src/CasinoJuego.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
+const CasinoJuegoSlot: React.FC<any> = (props) => {
+    return (
+        <div className="casino-juego-slot">
+            {props.bg}
+        </div>
+    )
+};
+
 const CasinoJuego: React.FC = () => {
     return (
-        <h1>Casino Juego</h1>
+        <div className="main-juego-container">
+            <div>
+                <h2>Casino Juego</h2>
+            </div>
+            <div className="caja-juego">
+                <CasinoJuegoSlot bg="xd" />
+                <CasinoJuegoSlot bg="ab" />
+                <CasinoJuegoSlot bg="cd" />
+                <CasinoJuegoSlot bg="ef" />
+            </div>
+            <div></div>
+            <div></div>
+        </div>
     )
 };
 
