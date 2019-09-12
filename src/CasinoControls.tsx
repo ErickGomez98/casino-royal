df52cd9a9de048451bdaf1c13ceac45e45e35ae3
Erick Gomez <erick0gomez98@gmail.com>
2019-09-11 20:50:25 -0500

    Se agregó la funcionalidad para cancelar monedas.

--- src/CasinoControls.tsx
+++ src/CasinoControls.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 
 type ItemProps = {
-    denominacion: number
+    denominacion: number,
+    active: boolean,
+    changeStatus: Function
 };
 
 
 const Moneda: React.FC<ItemProps> = (props) => {
     return (
-        <div className="moneda">
-            <div {...props} className="moneda-txt">{props.denominacion}</div>
+        <div className={props.active ? "moneda" : "moneda moneda-off"} onClick={() => props.changeStatus(props.denominacion)}>
+            <div className="moneda-txt" >{props.denominacion}</div>
         </div>
     )
 }
@@ -31,45 +33,83 @@ type CasinoControlsProps = {
     creditosTotales: number;
     controlarCreditos: Function;
 }
-const CasinoControls: React.FC<CasinoControlsProps> = (props) => {
-    return (
-        <div className="main-controles-container">
-            <div className="monedas-title">
-                <h2>Monedas disponibles</h2>
-            </div>
-            <div className="monedas-container">
-                <Moneda denominacion={2} />
-                <Moneda denominacion={5} />
-                <Moneda denominacion={10} />
-                <Moneda denominacion={20} />
-            </div>
-            <div className="divider"></div>
-            <div className="creditos-selector-container">
-                <h2>Cargar créditos</h2>
-                <CreditosSelectItem denominacion={1} controlarCreditos={(denominacion: number) => props.controlarCreditos(denominacion, 1)} />
-                <CreditosSelectItem denominacion={2} controlarCreditos={(denominacion: number) => props.controlarCreditos(denominacion, 1)} />
-                <CreditosSelectItem denominacion={5} controlarCreditos={(denominacion: number) => props.controlarCreditos(denominacion, 1)} />
-                <CreditosSelectItem denominacion={10} controlarCreditos={(denominacion: number) => props.controlarCreditos(denominacion, 1)} />
-            </div>
-            <div className="divider"></div>
-            <div className="creditos-totales-container">
-                <div>
-                    <h2>Créditos: </h2>
-                </div>
-                <div className="creditos-totales">{props.creditosTotales}</div>
-            </div>
-            <div className="divider"></div>
-            <div className="cobrar-creditos-container">
-                <div>
-                    <button type="button" className="boton-cobrar">Cobrar</button>
-                </div>
-                <div className="creditos-cobrados-result">
-                    creditos-cobrados
-                </div>
-            </div>
 
-        </div>
-    )
+interface IMoneda {
+    denominacion: number,
+    activo: boolean
+}
+
+interface ICasinoState {
+    monedas: Array<IMoneda>
+}
+class CasinoControls extends React.Component<CasinoControlsProps, {}> {
+    state: ICasinoState = {
+        monedas: [
+            {
+                denominacion: 2,
+                activo: true
+            },
+            {
+                denominacion: 5,
+                activo: true
+            },
+            {
+                denominacion: 10,
+                activo: true
+            },
+            {
+                denominacion: 20,
+                activo: true
+            }
+        ]
+    };
+
+    changeStatusMoneda = (denominacion: number) => {
+        this.setState({
+            monedas: this.state.monedas.map(moneda => (moneda.denominacion === denominacion ? { ...moneda, activo: !moneda.activo } : moneda))
+        });
+    }
+
+    render() {
+        return (
+            <div className="main-controles-container">
+                <div className="monedas-title">
+                    <h2>Monedas disponibles</h2>
+                </div>
+                <div className="monedas-container">
+                    {this.state.monedas.map(moneda => {
+                        return <Moneda key={moneda.denominacion} denominacion={moneda.denominacion} active={moneda.activo} changeStatus={(x: any) => this.changeStatusMoneda(x)} />
+                    })}
+                </div>
+                <div className="divider"></div>
+                <div className="creditos-selector-container">
+                    <h2>Cargar créditos</h2>
+                    <CreditosSelectItem denominacion={1} controlarCreditos={(denominacion: number) => this.props.controlarCreditos(denominacion, 1)} />
+                    <CreditosSelectItem denominacion={2} controlarCreditos={(denominacion: number) => this.props.controlarCreditos(denominacion, 1)} />
+                    <CreditosSelectItem denominacion={5} controlarCreditos={(denominacion: number) => this.props.controlarCreditos(denominacion, 1)} />
+                    <CreditosSelectItem denominacion={10} controlarCreditos={(denominacion: number) => this.props.controlarCreditos(denominacion, 1)} />
+                </div>
+                <div className="divider"></div>
+                <div className="creditos-totales-container">
+                    <div>
+                        <h2>Créditos: </h2>
+                    </div>
+                    <div className="creditos-totales">{this.props.creditosTotales}</div>
+                </div>
+                <div className="divider"></div>
+                <div className="cobrar-creditos-container">
+                    <div>
+                        <button type="button" className="boton-cobrar">Cobrar</button>
+                    </div>
+                    <div className="creditos-cobrados-result">
+                        creditos-cobrados
+                </div>
+                </div>
+
+            </div>
+        )
+    }
+
 };
 
 export default CasinoControls;
