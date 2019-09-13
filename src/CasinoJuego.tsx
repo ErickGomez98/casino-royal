6f1ed4d0722fcfd774fffeefd6df80c589eece02
Erick Gomez <erick0gomez98@gmail.com>
2019-09-12 22:25:50 -0500

    Se arregló el error que habia con la animación que debía de animar uno por uno y no todos al mismo tiempo.

--- src/CasinoJuego.tsx
+++ src/CasinoJuego.tsx
@@ -90,197 +90,62 @@ class CasinoJuego extends React.Component<CasinoJuegoProps, CasinoState> {
      */
     generarResultadosRandom = () => {
         let results: Array<Array<Number>> = [];
-        for (let i = 0; i < 10; i++) {
-            results.push(
-                [
+        for (let i = 0; i < 4; i++) {
+            let tmpResult: Array<Number> = [];
+            for (let j = 0; j < 30; j++) {
+                tmpResult.push(
                     Math.floor(Math.random() * 4) + 1,
-                    Math.floor(Math.random() * 4) + 1,
-                    Math.floor(Math.random() * 4) + 1,
-                    Math.floor(Math.random() * 4) + 1
-                ]
-            )
+                )
+            }
+
+            results.push(tmpResult);
         }
 
-        results = [
-            [1,1,2,2,1,4,1,2,3,2,1],
-            [2,3,4,2,3,4,1,2,3,4,2],
-            [4,2,1,2,3,2,4,1,2,3,2],
-            [3,2,1,4,1,2,2,3,4,2,1],
-        ];
+        let tmp: Array<Number> = [];
 
-        let xd: Array<Number> = [];
-
-        let aaa: Array<Array<Number>> = [];
+        let tmpResults: Array<Array<Number>> = [];
 
         results.map((v, k) => {
-            // la idea es que cada vuelta asi con ese array va a mostrar de cada slot por slot, y cuando finalice uno
-            // pues se deberia de quedar ese asi renderizado y no eliminarlo, ese es el pedo xd.
-            v.map((i, n) => {
-
-                xd[k] = i;
-                console.log(xd);
-                const qggg = [...xd];
-                //aaa = [...aaa, xd];
-                aaa.push(qggg)
-                
+            v.map(i => {
+                tmp[k] = i;
+                const tmpIntern = [...tmp];
+                tmpResults.push(tmpIntern)
             });
         });
 
-
-        // Ahora lo que vale aaa tiene todas las combinaciones, creo que ahora lo que tengo que hacer es un .map sobre aaa
-        // donde en cada vuelta del ciclo voy a hacer el tpl.push que ya tenia, de esta manera al inicio solo va a agregar
-        // un elemento a la vez, y al finalizar pues ya va a agregar todos los demas elementos pero los primeros tambien los
-        // estaria renderizando otra vez pero como el valor ya no se mueve entonces no debería de afectar.
-
-        // debera de ser un map adentro de un map, en el map grande se regresa el tpl completo al state, y en el map de adentro se va
-        // construyendo el tpl, tonces para sacar el timeout deberia de ser creo multiplicar los indices de cada map y sumarle los ms
-        // para el timeout
-
-        console.log(aaa);
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        for(let j = 0; j < 4; j++){
-            results.map((item, k) => {
-
-                let tpl: any = [];
-                if(j === 0){
-                    switch (item[j]) {
-                        case 1:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                            break;
-                        case 2:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={seven} />)
-                            break;
-                        case 3:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={cherry} />)
-                            break;
-                        case 4:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bar} />)
-                            break;
-                    }                    
-                }else if(j === 1){
-                    tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                    switch (item[j]) {
-                        case 1:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                            break;
-                        case 2:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={seven} />)
-                            break;
-                        case 3:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={cherry} />)
-                            break;
-                        case 4:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bar} />)
-                            break;
-                    }    
-                } else if (j === 2) {
-                    tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                    tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                    switch (item[j]) {
-                        case 1:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                            break;
-                        case 2:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={seven} />)
-                            break;
-                        case 3:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={cherry} />)
-                            break;
-                        case 4:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bar} />)
-                            break;
-                    }
-                } else if (j === 3) {
-                    tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                    tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                    tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                    switch (item[j]) {
-                        case 1:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
-                            break;
-                        case 2:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={seven} />)
-                            break;
-                        case 3:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={cherry} />)
-                            break;
-                        case 4:
-                            tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bar} />)
-                            break;
-                    }
-                }
-
-/*                
-                switch (item[j]) {
+        tmpResults.map((v, k) => {
+            let tpl: any = [];
+            v.map(vv => {
+                switch (vv) {
                     case 1:
-                        tpl.push(<CasinoJuegoSlot key={j} bg={bell} />)
+                        tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bell} />)
                         break;
                     case 2:
-                        tpl.push(<CasinoJuegoSlot key={j} bg={seven} />)
+                        tpl.push(<CasinoJuegoSlot key={Math.random()} bg={seven} />)
                         break;
                     case 3:
-                        tpl.push(<CasinoJuegoSlot key={j} bg={cherry} />)
+                        tpl.push(<CasinoJuegoSlot key={Math.random()} bg={cherry} />)
                         break;
                     case 4:
-                        tpl.push(<CasinoJuegoSlot key={j} bg={bar} />)
+                        tpl.push(<CasinoJuegoSlot key={Math.random()} bg={bar} />)
                         break;
                 }
-*/
-                /*
-                for (let i = 0; i < item.length; i++) {
-                    switch (item[i]) {
-                        case 1:
-                            tpl.push(<CasinoJuegoSlot key={i} bg={bell} />)
-                            break;
-                        case 2:
-                            tpl.push(<CasinoJuegoSlot key={i} bg={seven} />)
-                            break;
-                        case 3:
-                            tpl.push(<CasinoJuegoSlot key={i} bg={cherry} />)
-                            break;
-                        case 4:
-                            tpl.push(<CasinoJuegoSlot key={i} bg={bar} />)
-                            break;
-                    }
-                }
-                */
-                setTimeout(() => {
-                    this.setState({
-                        results: tpl
-                    });
-                }, j * 500);
             })
-        }
-        
+            setTimeout(() => {
+                this.setState({
+                    results: tpl
+                })
+
+            }, k * 100);
+        });
+
 
         setTimeout(() => {
             this.setState({
                 playing: false
             })
 
-            const lastResult = results[results.length - 1];
+            const lastResult = tmpResults[tmpResults.length - 1];
             let isResult: number = 0;
             for (let i = 0; i < lastResult.length; i++) {
                 let count = 0;
@@ -304,12 +169,12 @@ class CasinoJuego extends React.Component<CasinoJuegoProps, CasinoState> {
                     this.props.showSuccess("Has ganado 125 créditos!")
                 }
             }
-        }, (results.length) * 500);
+        }, (tmpResults.length) * 100);
     }
 
     render() {
         const { playing, results } = this.state;
-        
+
         return (
             <div className="main-juego-container">
                 <div>
@@ -320,8 +185,8 @@ class CasinoJuego extends React.Component<CasinoJuegoProps, CasinoState> {
                     <Palanca onClickJugar={this.jugar} playing={playing} />
                 </div>
 
-                <div style={{marginTop: 40, fontSize: 20}}>
-                    <div style={{fontSize: 30}}>PREMIOS</div>
+                <div style={{ marginTop: 40, fontSize: 20 }}>
+                    <div style={{ fontSize: 30 }}>PREMIOS</div>
                     <div>3 iguales: 85 créditos</div>
                     <div>4 iguales: 125 créditos</div>
                 </div>
